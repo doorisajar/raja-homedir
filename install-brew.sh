f39b1d95a0b8e9abb4ed064d58afbff3259afe97
Doake, Raja <raja.doake@nexteraanalytics.com>
2023-12-27 11:48:25 -0600

    Add some of my brew faves to install

--- install-brew.sh
+++ install-brew.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# upgrade installed formulae (if any)
+brew upgrade
+
+brew tap homebrew/cask
+
+# shell utils
+brew install exa
+brew install tmux
+brew install micro
+brew install tldr
+
+# file utils
+brew install jq
+brew install yq
+
+# programming language version managers
+brew install pyenv
+brew install juliaup
+
+brew tap r-lib/rig
+brew install --cask rig
+
+brew cleanup
