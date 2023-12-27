8c9d309d4f607ab11509200ea37a90583deb2807
Doake, Raja <raja.doake@nexteraanalytics.com>
2023-12-27 11:56:30 -0600

    more brew stuff

--- install-brew.sh
+++ install-brew.sh
@@ -8,18 +8,22 @@ brew upgrade
 brew tap homebrew/cask
 
 # shell utils
+brew install bat
 brew install exa
-brew install tmux
+brew install fzf
+brew install htop
+brew install hyperfine
 brew install micro
 brew install tldr
+brew install tmux
 
 # file utils
 brew install jq
 brew install yq
 
 # programming language version managers
-brew install pyenv
 brew install juliaup
+brew install pyenv
 
 brew tap r-lib/rig
 brew install --cask rig
