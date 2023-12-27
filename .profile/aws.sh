5b05504a7dd4f075cfe08584f0e6a755a7f31f96
Doake, Raja <raja.doake@nexteraanalytics.com>
2023-12-27 15:25:37 -0600

    Add AWS account checked

---
+++ .profile/aws.sh
@@ -0,0 +1,12 @@
+# pilfered from https://gitlab.windlogics.com/Jordan.Love/dotfiles
+
+# awsaccid: Get AWS account ID for default or specified profile
+awsaccount () {
+   if [[ $1 == "-p" ]]
+   then
+     profile=$2
+     aws sts get-caller-identity --output text --profile $profile| cut -f1
+   else
+     aws sts get-caller-identity --output text | cut -f1
+   fi     
+}
