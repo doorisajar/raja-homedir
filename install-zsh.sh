e0806ef0a12e5cf6d47b4b3d9864935bc80503b2
Raja Doake <rdoake@gmail.com>
2026-09-12 14:12:33 -0400

    Condition was backwards

--- install-zsh.sh
+++ install-zsh.sh
@@ -19,7 +19,7 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 
   # On Linux, need to ensure zsh is installed first
   if [[ $MACHINE == "Linux" ]]; then
-    if ! which zsh > /dev/null 2>&1; then
+    if which zsh > /dev/null 2>&1; then
       echo "ZSH not installed; can't install oh-my-zsh. Exiting."
       exit 1
     fi    
