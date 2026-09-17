74b30af5264a4f6c02317d392b86de3c2d75423b
Raja Doake <rdoake@gmail.com>
2026-09-17 10:29:19 -0400

    include pi profile for nono

--- install-configs.sh
+++ install-configs.sh
@@ -4,3 +4,8 @@ echo "Copying Julia config"
 
 mkdir -p ~/.julia/config
 cp configs/startup.jl ~/.julia/config/
+
+echo "Copying nono config"
+
+mkdir -p ~/.config/nono
+cp configs/pi.json ~/.config/nono/profiles
